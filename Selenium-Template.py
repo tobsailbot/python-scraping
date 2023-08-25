07189502aae743a9f9d9ad42e6e4cbc5eb06fbd3
Tobias Pilarche <tobsailbot@gmail.com>
2023-08-25 13:21:52 -0300

    startin point

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -12,8 +12,8 @@ import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 
 
-display = Display(visible=0, size=(800, 800))  
-display.start()
+# display = Display(visible=0, size=(800, 800))  
+# display.start()
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
@@ -41,50 +41,8 @@ driver = webdriver.Chrome(options = chrome_options)
 
 url = 'https://tienda.umbro.com.ar/botines/42?initialMap=c&initialQuery=botines&map=category-1,talle&order=OrderByPriceDESC'
 driver.get(url)
-is_available = False
-output = ""
 
 # Esperar a que la página cargue completamente
 titles = WebDriverWait(driver, 10).until(lambda x: x.find_elements(By.CLASS_NAME, "vtex-product-summary-2-x-productBrand"))
 prices = WebDriverWait(driver, 10).until(lambda x: x.find_elements(By.CLASS_NAME, "glamit-product-price-0-x-sellingPriceValue"))
 
-# Iterar sobre los elementos y obtener el innerHTML
-for i in range(len(titles)):
-    titles = WebDriverWait(driver, 10).until(lambda x: x.find_elements(By.CLASS_NAME, "vtex-product-summary-2-x-productBrand"))
-    if titles[i].text == "BOTIN SALA UMBRO PRO 5 BUMP" or titles[i].text == "BOTIN SINTETICO UMBRO SPECIALI III PRO":
-        output += "<h3>" + titles[i].text + "</h3>"
-        prices = WebDriverWait(driver, 10).until(lambda x: x.find_elements(By.CLASS_NAME, "glamit-product-price-0-x-sellingPriceValue"))
-        output += "<p>" + prices[i].text + "</p>"
-        output += "<br/>"
-        is_available = True
-
-
-output += f"<a href='{url}'>Ir a la tienda</a>"
-
-
-# ------------------------------------------------------
-
-# Configuración del servidor SMTP y de las credenciales
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
-SMTP_USERNAME = 'dentalturno@gmail.com'
-SMTP_PASSWORD = 'xcgyjoaatrrvyvhm'
-
-
-# Configuración del mensaje
-msg = MIMEMultipart()
-msg['From'] = '"Umbro Botines" <dentalturno@gmail.com>'
-msg['To'] = 'tobsailbot@gmail.com'
-msg['Subject'] = 'BOTINES DISPONIBLES - Umbro Web Scraping Selenium'
-
-print(is_available)
-if is_available:
-    # Agregar el cuerpo del mensaje
-    msg.attach(MIMEText(output, "html"))
-
-    # Enviar el correo electrónico
-    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-    server.starttls()
-    server.login(SMTP_USERNAME, SMTP_PASSWORD)
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
-    server.quit()
